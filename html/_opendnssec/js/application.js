8e4e9feff88541c56861932321a39643773ef85a
Jerry Lundström <lundstrom.jerry@gmail.com>
2013-07-30 15:24:11 +0200

    Outline base for OpenDNSSEC management console

---
+++ html/_opendnssec/js/application.js
@@ -0,0 +1,32 @@
+(function ($) {
+	$(function () {
+		window.lim.module.opendnssec = {
+			init: function () {
+				var that = this;
+				
+				$('.sidebar-nav a[href="#about"]').click(function () {
+					$('.sidebar-nav li').removeClass('active');
+					$(this).parent().addClass('active');
+					that.loadAbout();
+	    			return false;
+				});
+				
+				$('.sidebar-nav a[href="#"]').click(function () {
+					$('.sidebar-nav li').removeClass('active');
+					$(this).parent().addClass('active');
+	    			return false;
+				});
+				
+				this.loadAbout();
+			},
+			//
+			loadAbout: function () {
+				window.lim.loadPage('/_opendnssec/about.html')
+				.done(function (data) {
+					$('#opendnssec-content').html(data);
+				});
+			},
+		};
+		window.lim.module.opendnssec.init();
+	});
+})(window.jQuery);
